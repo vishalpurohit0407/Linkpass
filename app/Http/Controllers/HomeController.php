44679cc9938475805ca39a38aa37a74f18e59ad7
Mayur Tagadiya <tagadiyamayur@gmail.com>
2020-11-10 19:25:37 +0530

    Coming Soon Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth'); // Commented by Mayur for coming soon page
     }
 
     /**
@@ -26,31 +26,32 @@ class HomeController extends Controller
      */
     public function index()
     {
-        if(Auth::user()->zendesk_id != null){
-             //$tickets = Zendesk::users(Auth::user()->zendesk_id)->tickets()->requested();
-             $tickets = Zendesk::users(Auth::user()->zendesk_id)->tickets()->requested(['page' => 1, 'per_page' => 5, 'sort_by' => 'created_at', 'sort_order' => 'desc']);
-             $totalSupportTicket = $tickets->count;
-        }else{
-            $totalSupportTicket = 0;
-            $tickets = [];
-        }
+        // if(Auth::user()->zendesk_id != null){
+        //      //$tickets = Zendesk::users(Auth::user()->zendesk_id)->tickets()->requested();
+        //      $tickets = Zendesk::users(Auth::user()->zendesk_id)->tickets()->requested(['page' => 1, 'per_page' => 5, 'sort_by' => 'created_at', 'sort_order' => 'desc']);
+        //      $totalSupportTicket = $tickets->count;
+        // }else{
+        //     $totalSupportTicket = 0;
+        //     $tickets = [];
+        // }
        
         
-        $extensions = WarrantyExtension::where('user_id', Auth::user()->id)
-           ->whereIn('warranty_extension.status',['0','1','2'])
-           ->limit(5)
-           ->groupBy('unique_key')
-           ->orderBy('created_at','desc')
-           ->get();
+        // $extensions = WarrantyExtension::where('user_id', Auth::user()->id)
+        //    ->whereIn('warranty_extension.status',['0','1','2'])
+        //    ->limit(5)
+        //    ->groupBy('unique_key')
+        //    ->orderBy('created_at','desc')
+        //    ->get();
 
 
         
-        $totalWarrantyRequest = WarrantyExtension::where('user_id',Auth::user()->id)->groupBy('unique_key')->count();
-        $totalSelfDiagnosis = GuideCompletion::leftJoin('guide', 'guide_completion.guide_id', '=', 'guide.id')
-        ->where('guide_completion.user_id',Auth::user()->id)->where('guide.guide_type','self-diagnosis')->count();
-        $totalMaintenance = GuideCompletion::leftJoin('guide', 'guide_completion.guide_id', '=', 'guide.id')
-        ->where('guide_completion.user_id',Auth::user()->id)->where('guide.guide_type','maintenance')->count();
+        // $totalWarrantyRequest = WarrantyExtension::where('user_id',Auth::user()->id)->groupBy('unique_key')->count();
+        // $totalSelfDiagnosis = GuideCompletion::leftJoin('guide', 'guide_completion.guide_id', '=', 'guide.id')
+        // ->where('guide_completion.user_id',Auth::user()->id)->where('guide.guide_type','self-diagnosis')->count();
+        // $totalMaintenance = GuideCompletion::leftJoin('guide', 'guide_completion.guide_id', '=', 'guide.id')
+        // ->where('guide_completion.user_id',Auth::user()->id)->where('guide.guide_type','maintenance')->count();
 
-        return view('dashboard',array('totalSupportTicket' => $totalSupportTicket, 'totalWarrantyRequest' => $totalWarrantyRequest, 'totalSelfDiagnosis' => $totalSelfDiagnosis, 'totalMaintenance' => $totalMaintenance, 'extensions' => $extensions, 'tickets' => $tickets)); 
+        // return view('dashboard',array('totalSupportTicket' => $totalSupportTicket, 'totalWarrantyRequest' => $totalWarrantyRequest, 'totalSelfDiagnosis' => $totalSelfDiagnosis, 'totalMaintenance' => $totalMaintenance, 'extensions' => $extensions, 'tickets' => $tickets)); 
+        return view('coming_soon');
     }
 }
---
+++ resources/views/coming_soon.blade.php
@@ -0,0 +1,14 @@
+
+    <div class="header bg-gradient-primary py-7 py-lg-8">
+        <div class="container">
+            <div class="header-body text-center mt-7 mb-7">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12 col-md-12" style="text-align:center;">
+                        <img src="{{asset('assets/img/brand/coming_soon.png')}}" width="600" height="600" class="navbar-brand-img" alt="navbar-brand-img">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt--10 pb-5"></div>
