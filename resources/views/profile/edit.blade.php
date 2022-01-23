c15fafb45ac68d7a98e258d4b929b4a097f72f9e
Mayur <tagadiyamayur@gmail.com>
2022-01-23 23:47:23 +0530

    fixed tag

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -386,6 +386,17 @@ jQuery(document).ready(function($) {
         })
     });
 
+    $(document).mouseup(function(e)
+    {
+        var container = $('.tagsinput-box');
+
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!container.is(e.target) && container.has(e.target).length === 0)
+        {
+            hideTagsInput();
+        }
+    });
+
     $(document).on('click', '.addNewTag', function(event) {
         event.preventDefault();
         var groupId = $(this).data('group-id');
